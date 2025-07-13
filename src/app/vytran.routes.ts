8b962e3a52bfbfdcf79271fa4b580c0357f435fd
vytran1 <vy.tn171003@gmail.com>
2025-07-13 10:50:12 +0700

    Update - Add two file route for avoiding conflict

--- src/app/vytran.routes.ts
+++ src/app/vytran.routes.ts
@@ -1,15 +1,35 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { StartPageComponent } from './start-page/start-page.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { InventoryManagementComponent } from './inventory-management/inventory-management.component';
 import { ProductManagementComponent } from './product-management/product-management.component';
 import { CreateProductFormComponent } from './product-management/sub-components/create-product-form/create-product-form.component';
 import { ProductInformationComponent } from './product-management/sub-components/product-information/product-information.component';
-import { inventoryRoutes } from './vytran.routes';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  ...inventoryRoutes,
-  { path: '**', redirectTo: '/login', pathMatch: 'full' },
+export const inventoryRoutes: Routes = [
+  {
+    path: 'inventory',
+    component: StartPageComponent,
+    children: [
+      {
+        path: 'personal-info',
+        component: PersonalInfoComponent,
+      },
+      {
+        path: 'inventory-management',
+        component: InventoryManagementComponent,
+      },
+      {
+        path: 'product-management',
+        component: ProductManagementComponent,
+      },
+      {
+        path: 'create-product',
+        component: CreateProductFormComponent,
+      },
+      {
+        path: 'product-information/:productId',
+        component: ProductInformationComponent,
+      },
+    ],
+  },
 ];
